e0d45338048f437b6e22363d14555b9070ea66cb
FineTuxedo <artistadamsv@gmail.com>
2026-03-15 22:03:33 -0700

    did variable experimants

---
+++ Asvoboda_Hour7_P1/Assets/Scenes/scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    public int runSpeed;
+
+    void Start()
+    {
+        print("Start runs before an object Updates");
+    }
+
+    void Update()
+    {
+        // This is called once per frame
+    }
+}
