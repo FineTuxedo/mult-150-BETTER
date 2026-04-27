9b9d10409dc739c4ce7447206e8ca7e37357a1f0
FineTuxedo <artistadamsv@gmail.com>
2026-04-26 23:05:53 -0700

    made some balls

--- asvoboda_Hour8_P1/Assets/CameraControl.cs
+++ asvoboda_Hour8_P1/Assets/CameraControl.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get mouse movement
+        float xMove = Input.GetAxis("Mouse X");
+        float yMove = Input.GetAxis("Mouse Y");
+
+        // Translate the camera left/right (X) and up/down (Y)
+        // Adjust the .1f to change how fast the camera slides
+        transform.Translate(xMove * 0.1f, yMove * 0.1f, 0f);
     }
 }
+
